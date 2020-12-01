079a5cf067dd14449e45079ed9f93012f82a7f89
atweedTechtonic <anthony.tweed@techtonic.com>
2020-12-01 16:58:55 -0500

    display-wins-and-losses

--- exercises/coin-flipper-refactor-hooks/src/components/CoinFlipper.js
+++ exercises/coin-flipper-refactor-hooks/src/components/CoinFlipper.js
@@ -7,15 +7,19 @@ const CoinFlipper = () => {
   const [image, setImage] = useState(null);
   const [flip, setFlip] = useState('');
   const [call, setCall] = useState('');
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
 
   const callFlip = (e) => {
-    const call = e.target.getAttribute('data-call');
+    const call = e.target.getAttribute('data-call'); // getAttribute - gets value from button click
     const flip = Math.round(Math.random()) === 0 ? 'heads' : 'tails';
 
     setCall(call);
     setFlip(flip);
-    setImage(flip === 'heads' ? heads : tails);
+    setImage(flip === 'heads' ? heads : tails); // shows correct image for the flip
     setRoundCount(roundCount + 1);
+    setWins(call === flip ? wins + 1 : wins);
+    setLosses(call !== flip ? losses + 1 : losses);
   };
 
   return (
@@ -29,9 +33,12 @@ const CoinFlipper = () => {
           <h2>You {flip === call ? 'Won' : 'Lost'}!</h2>
         </>
       ) : (
+        // initial screen (when flip = '')
         <h2>No flip yet! Make your call below to start a round!</h2>
       )}
       <section>
+        <p>Wins: {wins}</p>
+        <p>Losses: {losses}</p>
         <button onClick={callFlip} data-call="heads">
           Call Heads!
         </button>{' '}
