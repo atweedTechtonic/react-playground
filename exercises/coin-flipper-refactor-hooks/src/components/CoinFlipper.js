7d5597867b01026d0f4ffa713d138b477ad095e4
atweedTechtonic <anthony.tweed@techtonic.com>
2020-12-01 16:49:41 -0500

    refactor-class-component-to-hooks

--- exercises/coin-flipper-refactor-hooks/src/components/CoinFlipper.js
+++ exercises/coin-flipper-refactor-hooks/src/components/CoinFlipper.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import heads from '../images/heads.png';
 import tails from '../images/tails.png';
 
-class CoinFlipper extends Component {
-  state = {
-    roundCount: 0,
-    image: null,
-    flip: '',
-    call: '',
-  };
+const CoinFlipper = () => {
+  const [roundCount, setRoundCount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [flip, setFlip] = useState('');
+  const [call, setCall] = useState('');
 
-  callFlip = (e) => {
+  const callFlip = (e) => {
     const call = e.target.getAttribute('data-call');
     const flip = Math.round(Math.random()) === 0 ? 'heads' : 'tails';
 
-    this.setState({
-      call,
-      flip,
-      image: flip === 'heads' ? heads : tails,
-      roundCount: this.state.roundCount + 1,
-    });
+    setCall(call);
+    setFlip(flip);
+    setImage(flip === 'heads' ? heads : tails);
+    setRoundCount(roundCount + 1);
   };
 
-  render() {
-    return (
-      <>
-        <h1>Welcome to Coin Flipper!</h1>
-        <p>Rounds so far: {this.state.roundCount}</p>
-        {this.state.flip ? (
-          <>
-            <img src={this.state.image} />
-            <h2>Your Call: {this.state.call}</h2>
-            <h2>You {this.state.flip === this.state.call ? 'Won' : 'Lost'}!</h2>
-          </>
-        ) : (
-          <h2>No flip yet! Make your call below to start a round!</h2>
-        )}
-        <section>
-          <button onClick={this.callFlip} data-call="heads">
-            Call Heads!
-          </button>{' '}
-          <button onClick={this.callFlip} data-call="tails">
-            Call Tails!
-          </button>
-        </section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>Welcome to Coin Flipper!</h1>
+      <p>Rounds so far: {roundCount}</p>
+      {flip ? (
+        <>
+          <img src={image} />
+          <h2>Your Call: {call}</h2>
+          <h2>You {flip === call ? 'Won' : 'Lost'}!</h2>
+        </>
+      ) : (
+        <h2>No flip yet! Make your call below to start a round!</h2>
+      )}
+      <section>
+        <button onClick={callFlip} data-call="heads">
+          Call Heads!
+        </button>{' '}
+        <button onClick={callFlip} data-call="tails">
+          Call Tails!
+        </button>
+      </section>
+    </>
+  );
+};
 
 export default CoinFlipper;
