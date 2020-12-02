12846a8688364db114263eadb167bd06d3235853
atweedTechtonic <anthony.tweed@techtonic.com>
2020-12-02 13:38:26 -0500

    add Spinner component

---
+++ exercises/hooks-giphy/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner fa-spin fa-3x" aria-hidden="true" />
+    </div>
+  );
+};
+
+export default Spinner;
